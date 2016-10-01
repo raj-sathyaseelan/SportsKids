29c16d019e7da3ef93afc81ba6f9baeacf64f7e9
Raj Sathyaseelan <RajSathya@ac02qm2rkg8wm.attlocal.net>
2016-10-01 11:31:05 -0700

    First commit

---
+++ KidsSports/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  KidsSports
+//
+//  Created by Raj Sathyaseelan on 9/25/16.
+//  Copyright © 2016 Token. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
